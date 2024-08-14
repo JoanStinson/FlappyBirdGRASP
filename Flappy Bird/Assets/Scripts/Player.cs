63ae21c20338c77d501bdda6af0280c89fd8a080
Joan Ginard Mateo <jgm7.hd@gmail.com>
2024-08-14 16:01:03 +0200

    Basic flap mechanic

--- Flappy Bird/Assets/Scripts/Player.cs
+++ Flappy Bird/Assets/Scripts/Player.cs
@@ -4,18 +4,33 @@ namespace JGM.Game
 {
     public class Player : MonoBehaviour
     {
+        [SerializeField] private Rigidbody2D m_rigidbody2D;
+        [SerializeField] private float m_flapStrength = 5f;
+
+        private bool m_shouldFlap;
+
         private void Update()
         {
             var playerInput = new PlayerInputBuilder().GetPlayerInput();
             if (playerInput.Pressed())
             {
-                OnTap();
+                m_shouldFlap = true;
             }
         }
 
-        private void OnTap()
+        private void FixedUpdate()
         {
-            Debug.Log("Tap or click detected!");
+            if (m_shouldFlap)
+            {
+                Flap();
+                m_shouldFlap = false;
+            }
+        }
+
+        private void Flap()
+        {
+            m_rigidbody2D.velocity = Vector2.zero;
+            m_rigidbody2D.AddForce(Vector2.up * m_flapStrength, ForceMode2D.Impulse);
         }
     }
 }
