f72c7debf2065ab709e5831b992ae2634f300c0b
Joan Ginard Mateo <jgm7.hd@gmail.com>
2024-08-14 20:25:28 +0200

    Starting pipes

---
+++ Flappy Bird/Assets/Scripts/Pipe/Pipe.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace JGM.Game
+{
+    public class Pipe : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.position -= Vector3.right * Time.deltaTime;
+        }
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/Pipe/PipeSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace JGM.Game
+{
+    public class PipeSpawner : MonoBehaviour
+    {
+        [SerializeField] private Transform m_pipesParent;
+        [SerializeField] private Pipe m_pipePrefab;
+
+        private void Start()
+        {
+            SpawnPipes();
+        }
+
+        private void SpawnPipes()
+        {
+            var pipeInstance = GameObject.Instantiate(m_pipePrefab, m_pipesParent, false);
+        }
+    }
+}
