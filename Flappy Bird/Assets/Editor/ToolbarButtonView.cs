ace9ff9270cb679028761bbe7e50e6f312ff2947
Joan Ginard Mateo <jgm7.hd@gmail.com>
2024-08-15 19:14:48 +0200

    Added default toolbar buttons

---
+++ Flappy Bird/Assets/Editor/ToolbarButtonView.cs
@@ -0,0 +1,167 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine.UIElements;
+
+namespace JGM.Editor
+{
+    [InitializeOnLoad]
+    public static class ToolbarButtonView
+    {
+        private const int m_buttonsOrderInToolbar = 2;
+
+        private static ScriptableObject m_currentToolbar = null;
+        private static Type m_toolbarType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.Toolbar");
+        private static VisualElement m_parent;
+        private static int m_lastInstanceID;
+
+        static ToolbarButtonView()
+        {
+            EditorApplication.update -= OnUpdate;
+            EditorApplication.update += OnUpdate;
+        }
+
+        private static void OnUpdate()
+        {
+            SetCurrentToolbar();
+            RecreateGUI();
+            SetToolbar();
+        }
+
+        private static void SetCurrentToolbar()
+        {
+            if (m_currentToolbar != null)
+            {
+                return;
+            }
+
+            var toolbars = Resources.FindObjectsOfTypeAll(m_toolbarType);
+            m_currentToolbar = (toolbars.Length > 0) ? (ScriptableObject)toolbars[0] : null;
+        }
+
+        private static void RecreateGUI()
+        {
+            if (!ShouldRecreateGUI())
+            {
+                return;
+            }
+
+            m_parent.RemoveFromHierarchy();
+            m_parent = null;
+            m_lastInstanceID = m_currentToolbar.GetInstanceID();
+        }
+
+        private static bool ShouldRecreateGUI()
+        {
+            return m_currentToolbar != null && m_parent != null && m_currentToolbar.GetInstanceID() != m_lastInstanceID;
+        }
+
+        private static void SetToolbar()
+        {
+            if (!ShouldSetToolbar())
+            {
+                return;
+            }
+
+            var root = m_currentToolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (root == null)
+            {
+                return;
+            }
+
+            var rawRoot = root.GetValue(m_currentToolbar);
+            if (rawRoot == null)
+            {
+                return;
+            }
+
+            m_parent?.RemoveFromHierarchy();
+            m_parent = null;
+            m_parent = new VisualElement()
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    alignItems = Align.FlexStart,
+                    justifyContent = Justify.FlexStart
+                }
+            };
+
+            var mRoot = rawRoot as VisualElement;
+            var toolbarZoneLeftAlign = mRoot.Q("ToolbarZoneLeftAlign");
+            toolbarZoneLeftAlign.Insert(m_buttonsOrderInToolbar, m_parent);
+            OnAttachToToolbar(m_parent);
+        }
+
+        private static bool ShouldSetToolbar()
+        {
+            return m_currentToolbar != null && m_parent == null;
+        }
+
+        private static void OnAttachToToolbar(VisualElement parent)
+        {
+            var methods = TypeCache.GetMethodsWithAttribute<ToolbarButtonAttribute>();
+            var allAttributes = new Dictionary<MethodInfo, ToolbarButtonAttribute>();
+
+            foreach (var method in methods)
+            {
+                var attribute = (ToolbarButtonAttribute)method.GetCustomAttributes(typeof(ToolbarButtonAttribute), false).First();
+                if (attribute != null)
+                {
+                    allAttributes.Add(method, attribute);
+                }
+            }
+
+            foreach (var attr in allAttributes.OrderByDescending(x => x.Value.order))
+            {
+                parent.Add(CreateToolbarButton(attr.Value.iconName, () => attr.Key.Invoke(null, null), attr.Value.tooltip));
+            }
+        }
+
+        private static VisualElement CreateToolbarButton(string icon, Action onClick, string tooltip = null)
+        {
+            Button buttonVE = new Button(onClick);
+            buttonVE.tooltip = tooltip;
+            FitChildrenStyle(buttonVE);
+
+            VisualElement iconVE = new VisualElement();
+            iconVE.AddToClassList("unity-editor-toolbar-element__icon");
+#if UNITY_2021_2_OR_NEWER
+            iconVE.style.backgroundImage = Background.FromTexture2D((Texture2D)EditorGUIUtility.IconContent(icon).image);
+            iconVE.style.height = 16;
+            iconVE.style.width = 16;
+            iconVE.style.alignSelf = Align.Center;
+#else
+            iconVE.style.backgroundImage = Background.FromTexture2D(EditorGUIUtility.FindTexture(icon));
+#endif
+            buttonVE.Add(iconVE);
+
+            return buttonVE;
+        }
+
+        private static void FitChildrenStyle(VisualElement element)
+        {
+            element.AddToClassList("unity-toolbar-button");
+            element.AddToClassList("unity-editor-toolbar-element");
+            element.RemoveFromClassList("unity-button");
+#if UNITY_2021_2_OR_NEWER
+            element.style.paddingRight = 8;
+            element.style.paddingLeft = 8;
+            element.style.justifyContent = Justify.Center;
+            element.style.display = DisplayStyle.Flex;
+            element.style.borderTopLeftRadius = 2;
+            element.style.borderTopRightRadius = 2;
+            element.style.borderBottomLeftRadius = 2;
+            element.style.borderBottomRightRadius = 2;
+            element.style.marginRight = 2;
+            element.style.marginLeft = 2;
+#else
+            element.style.marginRight = 2;
+            element.style.marginLeft = 2;
+#endif
+        }
+    }
+}
---
+++ Flappy Bird/Assets/Editor/ToolbarButtons.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System;
+using UnityEditor.IMGUI.Controls;
+using UnityEditor.ShortcutManagement;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEditor.SceneManagement;
+
+namespace JGM.Editor
+{
+    public static class ToolbarButtons
+    {
+        private const string scenesFolder = "Scenes";
+        private static AdvancedDropdownState scenesState = new AdvancedDropdownState();
+
+        public static T[] GetAtPath<T>(string path)
+        {
+            ArrayList al = new ArrayList();
+            string[] fileEntries = Directory.GetFiles(Application.dataPath + "/" + path);
+
+            foreach (string fileName in fileEntries)
+            {
+                string temp = fileName.Replace("\\", "/");
+                int index = temp.LastIndexOf("/");
+                string localPath = "Assets/" + path;
+
+                if (index > 0)
+                    localPath += temp.Substring(index);
+
+                System.Object t = AssetDatabase.LoadAssetAtPath(localPath, typeof(T));
+
+                if (t != null)
+                    al.Add(t);
+            }
+
+            T[] result = new T[al.Count];
+
+            for (int i = 0; i < al.Count; i++)
+                result[i] = (T)al[i];
+
+            return result;
+        }
+
+        [ToolbarButton("d_winbtn_win_max", "Open Terminal")]
+        public static void OpenTerminal()
+        {
+            var projectPath = Directory.GetParent(Application.dataPath).FullName;
+
+            Process cmd = new Process();
+#if UNITY_EDITOR_WIN
+            cmd.StartInfo.FileName = "cmd.exe";
+#endif
+#if UNITY_EDITOR_OSX
+            cmd.StartInfo.FileName = "/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal";
+#endif
+            cmd.StartInfo.WorkingDirectory = projectPath;
+            cmd.Start();
+        }
+
+        [ToolbarButton("Folder Icon", "Open Folder")]
+        public static void OpenFolder()
+        {
+            var projectPath = Directory.GetParent(Application.dataPath).FullName;
+
+            Process cmd = new Process();
+#if UNITY_EDITOR_WIN
+            cmd.StartInfo.FileName = "explorer.exe";
+#endif
+            // #if UNITY_EDITOR_OSX
+            //             cmd.StartInfo.FileName = "/bin/bash";
+            // #endif
+            cmd.StartInfo.Arguments = projectPath;
+            cmd.Start();
+        }
+
+#if UNITY_EDITOR_WIN
+        [ToolbarButton("UnityEditor.VersionControl", "Open Fork")]
+        public static void OpenFork()
+        {
+            var projectPath = Directory.GetParent(Application.dataPath).FullName;
+            var forkPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = forkPath + "\\Fork\\Fork.exe";
+
+            cmd.StartInfo.Arguments = projectPath;
+            cmd.Start();
+        }
+#endif
+
+        [ToolbarButton(iconName = "Package Manager", tooltip = "Package Manager")]
+        public static void ShowPackageManager()
+        {
+            UnityEditor.PackageManager.UI.Window.Open("");
+        }
+
+
+        [ToolbarButton("Settings", "Show Settings")]
+        public static void ShowSettings()
+        {
+            var a = new GenericMenu();
+            a.AddItem(new GUIContent("Project"), false, () => EditorApplication.ExecuteMenuItem("Edit/Project Settings..."));
+            a.AddItem(new GUIContent("Preferences"), false, () => EditorApplication.ExecuteMenuItem("Edit/Preferences..."));
+            a.ShowAsContext();
+        }
+
+        public static string ReplaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+
+        public static string ReplaceLast(string Source, string Find, string Replace)
+        {
+            int place = Source.LastIndexOf(Find);
+
+            if (place == -1)
+                return Source;
+
+            string result = Source.Remove(place, Find.Length).Insert(place, Replace);
+            return result;
+        }
+
+        //[ToolbarButton("UnityEditor.GameView", "Show Bootstrap Scene")]
+        //public static void ShowBootstrapScene()
+        //{
+        //    var bootstrapPath = "Assets/" + scenesFolder + "/bootstrap.unity";
+        //    if (!Application.isPlaying && File.Exists(bootstrapPath))
+        //        EditorSceneManager.OpenScene(bootstrapPath, OpenSceneMode.Additive);
+        //    Selection.activeGameObject = GameObject.FindGameObjectWithTag("Player");
+        //    SceneView.FrameLastActiveSceneView();
+        //}
+
+        public static void ShowQuickSearch()
+        {
+            EditorApplication.ExecuteMenuItem("Help/Quick Search");
+        }
+    }
+}
