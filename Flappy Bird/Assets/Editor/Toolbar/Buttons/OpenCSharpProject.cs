b791f02002c7bfa863c75a513f0296a38df18543
Joan Ginard Mateo <jgm7.hd@gmail.com>
2024-08-17 13:23:21 +0200

    Added open c# project button

---
+++ Flappy Bird/Assets/Editor/Toolbar/Buttons/OpenCSharpProject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JGM.Editor
+{
+    public static class OpenCSharpProject
+    {
+        [ToolbarButton(IconName = "UnityEditor.ConsoleWindow", Tooltip = "Open C# Project", Order = -100)]
+        public static void OnButtonClick()
+        {
+            AssetDatabase.Refresh();
+            UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(Application.dataPath + "/../" + Application.productName + ".sln", 0);
+        }
+    }
+}
